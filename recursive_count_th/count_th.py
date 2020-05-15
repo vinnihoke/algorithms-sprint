1f41567481d999124cf0d6693e23da75a133e5a1
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-15 14:45:53 -0700

    Added notes

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -13,15 +13,32 @@ def helper(idx1, idx2, message, sleep):
 
 
 def count_th(word):
+    # Break the arr into a list of individual characters
     arr = list(word)
+
+    # If the list is empty, return 0
     if not arr:
         return 0
+
+    # If len of arr is greater than 1
     elif len(arr) > 1:
+
+        # If arr[0] is "t" and arr[1] is "h"
         if arr[0] == "t" and arr[1] == "h":
+
+            # Helper
             helper(arr[0], arr[1], "Match", .5)
+
+            # Return 1 + a re-run of this function that takes off the first letter of the arr. This will re-run until a break case, and return this final value.
             return 1 + count_th("".join(arr[1:]))
         else:
+
+            # Helper
             helper(arr[0], arr[1], "Not", .5)
+
+            # Return 0 + a re-run of this function that takes off the first letter of the arr. This will re-run until a break case, and return this final value.
             return 0 + count_th("".join(arr[1:]))
+
+    # If array is less than 1, there are no more possible matches. Return 0.
     else:
         return 0
